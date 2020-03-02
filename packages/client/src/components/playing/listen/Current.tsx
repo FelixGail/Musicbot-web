644fb6175806111760a413a9695cbcf7924496a7
Felix Gail <felix.clemens.gail@gmail.com>
2020-03-02 16:02:17 +0100

    Feature/fullscreen (#117)
    * remove unused files
    
    * implement fullscreen (close #18)
    
    * Add missing FullscreenContext file

--- packages/client/src/components/playing/listen/Current.tsx
+++ packages/client/src/components/playing/listen/Current.tsx
@@ -3,20 +3,27 @@ import { AlbumArt } from "../snippets/AlbumArt";
 import React, { useMemo } from "react";
 import ScreenNavigation from "../../util/ScreenNavigation";
 import { useLocation } from "react-use";
+import { useContext } from "react";
+import { FullscreenContext } from "../../../core/context/FullscreenContext";
 
 const Current = (props: { song?: Song }) => {
-  const location = useLocation()
+  const location = useLocation();
+  const toggleFullscreen = useContext(FullscreenContext);
+  const jsx = useMemo(
+    () => (
+      <div className="current full-width full-height vertically-centering centering">
+        <AlbumArt song={props.song} />
+        <ScreenNavigation
+          left={`${location.pathname}/history`}
+          right={`${location.pathname}/queue`}
+          center={toggleFullscreen}
+        />
+      </div>
+    ),
+    [props.song, location.pathname, toggleFullscreen]
+  );
 
-  const jsx = useMemo(() => (
-    <div className="current full-width full-height vertically-centering centering">
-      <AlbumArt song={props.song} />
-      <ScreenNavigation
-        left={`${location.pathname}/history`}
-        right={`${location.pathname}/queue`}
-      />
-    </div>), [props.song, location.pathname])
-
-    return jsx;
+  return jsx;
 };
 
 export default Current;
--- packages/client/src/components/playing/listen/ListenRouter.tsx
+++ packages/client/src/components/playing/listen/ListenRouter.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useMemo, useRef } from "react";
 import { Layout } from "antd";
 import { Route } from "react-router";
 import "moment-duration-format";
@@ -8,28 +8,49 @@ import Queue from "./Queue";
 import ListenFooter from "./ListenFooter";
 import { useResourceReload } from "../../../core/hooks/usePlayerStateContext";
 import api from "../../../core/api/model";
+import { useToggle, useFullscreen } from "react-use";
+import { FullscreenContext } from "../../../core/context/FullscreenContext";
 
 export const ListenRouter = () => {
-  const playerState = useResourceReload(api.getPlayerState, undefined)
+  const playerState = useResourceReload(api.getPlayerState, undefined);
+  const [showFullscreen, toggleFullscreen] = useToggle(false);
+  const ref = useRef(null);
+  const isFullscreen = useFullscreen(ref, showFullscreen, {
+    onClose: () => toggleFullscreen(false)
+  });
 
   const renderCurrent = useCallback(
-    () => (
-     playerState && <Current song={playerState.songEntry && playerState.songEntry.song}/>
-    ),
+    () =>
+      playerState && (
+        <Current song={playerState.songEntry && playerState.songEntry.song} />
+      ),
     [playerState]
   );
 
-  const jsx = useMemo(() => (
-  <div className="currently-playing">
-    <Layout>
-      <Layout.Content>
-        <Route exact path="*/listen" render={renderCurrent} />
-        <Route path="*/listen/history" component={History} />
-        <Route path="*/listen/queue" component={Queue} />
-      </Layout.Content>
-      {playerState && <ListenFooter current={playerState}/>}
-    </Layout>
-  </div>), [renderCurrent, playerState])
+  const jsx = useMemo(
+    () => (
+      <div className="currently-playing">
+        <FullscreenContext.Provider value={toggleFullscreen}>
+          <div ref={ref} className="fullscreen">
+            <Layout>
+              <Layout.Content>
+                <Route exact path="*/listen" render={renderCurrent} />
+                <Route path="*/listen/history" component={History} />
+                <Route path="*/listen/queue" component={Queue} />
+              </Layout.Content>
+              {playerState && (
+                <ListenFooter
+                  current={playerState}
+                  showActions={!isFullscreen}
+                />
+              )}
+            </Layout>
+          </div>
+        </FullscreenContext.Provider>
+      </div>
+    ),
+    [renderCurrent, playerState, toggleFullscreen, ref, isFullscreen]
+  );
 
   return jsx;
 };
