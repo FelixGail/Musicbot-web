3d1c00a024371c932b75c8f202ca9e61e5e47bbd
Felix Gail <felix.clemens.gail@gmail.com>
2020-04-07 15:06:49 +0200

    Make Search/Suggest tabs full width

--- packages/client/src/components/playing/add/Suggest.tsx
+++ packages/client/src/components/playing/add/Suggest.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import React from "react";
 import { Row, Tabs } from "antd";
 import { SuggesterPane } from "../snippets/SongPanes";
+import styled from "styled-components";
 
 const Suggest = () => {
   const [suggesters, getSuggesters] = useResource(api.getSuggesters);
@@ -12,20 +13,22 @@ const Suggest = () => {
     getSuggesters();
   }, [getSuggesters]);
 
-  return (
-    <div className="suggest">
-      <Row>
-        {suggesters.data && (
-          <Tabs className="spanning">
+  const StyledTabs = styled(Tabs)`
+    width: 100%;
+  `;
+
+  if(!suggesters.data) {
+    return null
+  }
+
+  return (<StyledTabs>
             {suggesters.data.map((suggester, _) => (
               <Tabs.TabPane tab={suggester.name} key={suggester.id}>
                 <SuggesterPane suggester={suggester} />
               </Tabs.TabPane>
             ))}
-          </Tabs>
-        )}
-      </Row>
-    </div>
+          </StyledTabs>
+        
   );
 };
 
