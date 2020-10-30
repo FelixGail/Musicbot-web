0dae2a8bebd65a3108afd79824b92bec2d933f43
Felix Gail <felix.clemens.gail@gmail.com>
2020-10-30 14:58:14 +0100

    make carouselslick 5px

--- src/components/playing/footer/CarouselSlick.tsx
+++ src/components/playing/footer/CarouselSlick.tsx
@@ -2,7 +2,7 @@ import { Col } from "antd";
 import styled from "styled-components";
 
 export const CarouselSlick = styled(Col)`
-    height: 3px;
+    height: 5px;
     flex: 0 0 auto;
     background-color: #1890ff;
 `;
